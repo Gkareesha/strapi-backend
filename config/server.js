f61d19ed96709253f5a5ceee262dd938dd86ef38
Corina <k.gorodetskaya@icloud.com>
2022-02-09 23:11:58 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8ba0c735ceb0c3ca2085ce910dc2df72'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e138bc20-5d98-4708-b28d-cd03384aaa1f'
+};
